4789793fa6faf95447bf9994ebb8710076f51bdc
babloo <bhuvaneshaashi@gmail.com>
2026-05-25 13:52:49 -0400

    potal name update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/employee-timesheet-portal/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/aasys-employee-portal-frontend/' : '/',
 })
